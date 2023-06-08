f6db23d9f49825b970949fa0e2d371e2b6090e78
willianbarata <willianbarata@gmail.com>
2023-06-08 13:30:04 -0300

    Concluindo projeto número da sorte

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -1,24 +1,38 @@
-﻿namespace AppNumeroDaSorte;
+namespace AppNumeroDaSorte;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnGenerateLuckNumbers(object sender, EventArgs e)
 	{
-		count++;
+		NameApp.IsVisible = false;
+		ContainerLuckNumbers.IsVisible = true;
+		var set = GenerateLuckNumbers();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		LuckNumber01.Text = set.ElementAt(0).ToString("D2");
+        LuckNumber02.Text = set.ElementAt(1).ToString("D2");
+        LuckNumber03.Text = set.ElementAt(2).ToString("D2");
+        LuckNumber04.Text = set.ElementAt(3).ToString("D2");
+        LuckNumber05.Text = set.ElementAt(4).ToString("D2");
+        LuckNumber06.Text = set.ElementAt(5).ToString("D2");
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+	private SortedSet<int> GenerateLuckNumbers()
+	{
+		var set = new SortedSet<int>();
+
+		while(set.Count < 6)
+		{
+            var random = new Random();
+            var luckNumber = random.Next(1, 60); 
+			set.Add(luckNumber);
+		}
+
+		return set;
 	}
 }
-
